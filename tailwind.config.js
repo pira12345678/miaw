9357a9492cdf364d30326afd05c8cabe7d3ba477
pira12345678 <firaadzlia@gmail.com>
2026-03-05 13:40:21 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        Noto: ["Noto Sans", "sans-serif"],
+      },
+    },
+  },
+};
